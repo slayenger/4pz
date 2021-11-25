2f1247a8430157d4eaa678f2b88328c0f40add2d
slayenger <sinyavskiym@mail.ru>
2021-11-25 13:20:21 +0300

    Map

---
+++ 4thpz_map.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+
+    map <string, int> mapp;
+    string text;
+    string word;
+
+    cout << "Enter text: " << endl;
+
+    getline(cin, text);
+    stringstream ss(text);
+
+    while (getline(ss, word, ' '))
+    {
+        if (mapp.find(word) == mapp.end())
+        {
+            mapp.emplace(word, 1);
+        }
+        else
+        {
+            mapp[word] ++;
+        }
+    }
+    mapp.erase("");
+    auto iter = mapp.begin();
+    cout << endl;
+    for (int i = 1; iter != mapp.end(); i++, iter++)
+    { 
+        cout << i <<  "   " << iter->first << " - " << iter->second << endl;            
+    }
+
+    return 0;
+}
